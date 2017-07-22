8974f17e58f1eb57b7fa165c695085b3cc1fb5f9
James Ko <jamesqko@gmail.com>
2017-07-22 17:25:48 -0400

    [SmallList] Add XML docs

--- src/BlockList/SmallList_1.cs
+++ src/BlockList/SmallList_1.cs
@@ -5,18 +5,47 @@ using System.Diagnostics;
 
 namespace Clever.Collections
 {
-    // TODO: Add XML docs. Explain why it's called SmallList.
+    /// <summary>
+    /// A list optimized for a small number of items.
+    /// </summary>
+    /// <typeparam name="T">The type of the items.</typeparam>
     internal struct SmallList<T>
     {
+        /// <summary>
+        /// The size of this list's buffer after the first item is added.
+        /// </summary>
         private const int InitialCapacity = 4;
 
+        /// <summary>
+        /// The buffer where this list's items are stored.
+        /// </summary>
         private T[] _array;
+
+        /// <summary>
+        /// The number of items in this list.
+        /// </summary>
         private int _count;
 
+        /// <summary>
+        /// Gets the number of items this list can hold before resizing.
+        /// </summary>
         public int Capacity => _array?.Length ?? 0;
 
+        /// <summary>
+        /// Gets the number of items in this list.
+        /// </summary>
         public int Count => _count;
 
+        /// <summary>
+        /// Gets a value indicating whether this list is full.
+        /// </summary>
+        private bool IsFull => _count == Capacity;
+
+        /// <summary>
+        /// Gets or sets an item in this list.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns>The item at the specified index.</returns>
         public T this[int index]
         {
             get
@@ -31,9 +60,13 @@ namespace Clever.Collections
             }
         }
 
+        /// <summary>
+        /// Adds an item to this list.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
         public void Add(T item)
         {
-            if (_count == Capacity)
+            if (IsFull)
             {
                 MakeRoom();
             }
@@ -41,9 +74,12 @@ namespace Clever.Collections
             _array[_count++] = item;
         }
 
+        /// <summary>
+        /// Resizes this list when it is full.
+        /// </summary>
         private void MakeRoom()
         {
-            Debug.Assert(_count == Capacity);
+            Debug.Assert(IsFull);
 
             T[] newArray;
             if (_count == 0)
