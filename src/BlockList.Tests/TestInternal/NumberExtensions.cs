b846917803a37eaa12bee6b09894e0bcffc9e184
James Ko <jamesqko@gmail.com>
2017-07-23 17:41:47 -0400

    Forgot to commit a file

---
+++ src/BlockList.Tests/TestInternal/NumberExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clever.Collections.Tests.TestInternal
+{
+    internal static class NumberExtensions
+    {
+        public static bool IsInteger(this double d) => d % 1 == 0;
+
+        public static int Pow(this int @base, int exponent)
+        {
+            int result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= @base;
+            }
+            return result;
+        }
+    }
+}
