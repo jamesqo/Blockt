39ddf8279c978638c245708b6a4fc7509c07e2d8
James Ko <jamesqko@gmail.com>
2017-07-23 18:43:43 -0400

    [Tests] Add coverage for BlockList.Options.Equals

--- src/BlockList/BlockList.cs
+++ src/BlockList/BlockList.cs
@@ -3,7 +3,7 @@
     public static partial class BlockList
     {
         public static Options DefaultOptions { get; } =
-            CreateOptions(initialCapacity: 32);
+            CreateOptions(initialCapacity: 32); // TODO: Change to 8
 
         public static Options CreateOptions(int initialCapacity)
         {
