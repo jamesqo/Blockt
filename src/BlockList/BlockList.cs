d97d63ff8548931e9ecc31580361a25fbb0c4d7c
James Ko <jamesqko@gmail.com>
2017-07-23 22:46:00 -0400

    Change BlockList.InitialCapacity to a more modest 8

--- src/BlockList/BlockList.cs
+++ src/BlockList/BlockList.cs
@@ -2,12 +2,8 @@
 {
     public static partial class BlockList
     {
-        public static Options DefaultOptions { get; } =
-            CreateOptions(initialCapacity: 32); // TODO: Change to 8
+        public static Options DefaultOptions { get; } = CreateOptions(initialCapacity: 8);
 
-        public static Options CreateOptions(int initialCapacity)
-        {
-            return new Options(initialCapacity);
-        }
+        public static Options CreateOptions(int initialCapacity) => new Options(initialCapacity);
     }
 }
